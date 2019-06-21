71d876aef888ae27483b7a1b9a3be2c4a9364030
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2019-06-21 10:11:01 -0400

    initial app

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path')
 const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ScriptExtHtmlWebpackPlugin = require('script-ext-html-webpack-plugin')
 
 module.exports = {
   entry: path.join(__dirname, 'src/index.js'),
@@ -21,8 +23,17 @@ module.exports = {
   },
   plugins: [
     // make sure to include the plugin!
-    new VueLoaderPlugin()
+    new VueLoaderPlugin(),
+    // process HTML
+    new HtmlWebpackPlugin({
+      title: 'Custom template',
+      template: path.join(__dirname, 'src/index.template.html')
+    }),
+    new ScriptExtHtmlWebpackPlugin({
+      defaultAttribute: 'defer'
+    })
   ],
+  resolve: { alias: { vue: 'vue/dist/vue.esm.js' } },
   stats: {
     colors: true
   },
