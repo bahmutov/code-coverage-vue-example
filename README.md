0b694bb9fbae387bbb12d74bd767bd308c34c248
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2019-06-27 16:06:43 -0400

    add screenshots

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
 # code-coverage-vue-example [![renovate-app badge][renovate-badge]][renovate-app] [![CircleCI](https://circleci.com/gh/bahmutov/code-coverage-vue-example/tree/master.svg?style=svg)](https://circleci.com/gh/bahmutov/code-coverage-vue-example/tree/master) [![Coverage Status](https://coveralls.io/repos/github/bahmutov/code-coverage-vue-example/badge.svg)](https://coveralls.io/github/bahmutov/code-coverage-vue-example)
 > Code coverage for Vue single file components from Cypress.io end-to-end tests
 
-Uses [@cypress/code-coverage](https://github.com/cypress-io/code-coverage) plugin for [Cypress.io](https://www.cypress.io) test runner. See [https://on.cypress.io/code-coverage](https://on.cypress.io/code-coverage)
+Uses [@cypress/code-coverage](https://github.com/cypress-io/code-coverage) plugin for [Cypress.io](https://www.cypress.io) test runner. See [https://on.cypress.io/code-coverage](https://on.cypress.io/code-coverage) documentation.
+
+![Total coverage](images/coverage.png)
+
+Individual `.vue` file coverage
+
+![Vue file coverage](images/component-coverage.png)
 
 Because this application is served using `webpack-dev-server, it instruments the application code following the example in [bahmutov/code-coverage-webpack-dev-server](https://github.com/bahmutov/code-coverage-webpack-dev-server) via [.babelrc](.babelrc) and [webpack.config.js](webpack.config.js) files.
 
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,24 +1,7 @@
-<template>
-  <div>
-    <BaseInputText
-      data-cy="input"
-      v-model="newTodoText"
-      placeholder="New todo"
-      @keydown.enter="addTodo"
-    />
-    <ul v-if="todos.length">
-      <TodoListItem v-for="todo in todos" :key="todo.id" :todo="todo" @remove="removeTodo"/>
-    </ul>
-    <p v-else>Nothing left in the list. Add a new todo in the input above.</p>
-  </div>
-</template>
-
 <script>
 import BaseInputText from "./BaseInputText.vue";
 import TodoListItem from "./TodoListItem.vue";
-
 let nextTodoId = 1;
-
 export default {
   components: {
     BaseInputText,
@@ -27,23 +10,8 @@ export default {
   data() {
     return {
       newTodoText: "",
-      todos: [
-        // empty list at first
-        /*
-        {
-          id: nextTodoId++,
-          text: "Learn Vue"
-        },
-        {
-          id: nextTodoId++,
-          text: "Learn about single-file components"
-        },
-        {
-          id: nextTodoId++,
-          text: "Fall in love"
-        }
-        */
-      ]
+      // empty list at first, each item like with "id" and "text"
+      todos: []
     };
   },
   methods: {
@@ -65,3 +33,18 @@ export default {
   }
 };
 </script>
+
+<template>
+  <div>
+    <BaseInputText
+      data-cy="input"
+      v-model="newTodoText"
+      placeholder="New todo"
+      @keydown.enter="addTodo"
+    />
+    <ul v-if="todos.length">
+      <TodoListItem v-for="todo in todos" :key="todo.id" :todo="todo" @remove="removeTodo"/>
+    </ul>
+    <p v-else>Nothing left in the list. Add a new todo in the input above.</p>
+  </div>
+</template>
